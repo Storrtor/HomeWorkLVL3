3de065277d3662e3a4e0d3dbcf44433e708e22a0
Storrtor <storrtor@gmail.com>
2021-06-07 16:30:43 +0300

    End

---
+++ src/main/java/HomeWork5/MainClass.java
@@ -0,0 +1,29 @@
+package HomeWork5;
+
+import java.util.ArrayList;
+
+public class MainClass {
+    public static final int CARS_COUNT = 4;
+
+    public static void main(String[] args) {
+        System.out.println("Важное объявление >>> Подготовка!!!");
+        Race race = new Race(new Road(60), new Tunnel(), new Road(40));
+        Car[] cars = new Car[CARS_COUNT];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = new Car(race, 20 + (int) (Math.random() * 10));
+        }
+        for (int i = 0; i < cars.length; i++) {
+            new Thread(cars[i]).start();
+        }
+        System.out.println("Важное обьявление >>> Гонка началась!!!");
+        System.out.println("Важное обьявление >>> Гонка закончилась!!!");
+
+
+    }
+
+
+
+
+
+
+}
