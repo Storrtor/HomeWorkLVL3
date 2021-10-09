47305ba320bed117a05562ae2fc7068633f4310d
stortor <storrtor@gmail.com>
2021-10-09 13:13:09 +0300

    End

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        Numbers(5, 4);
+    }
+    public static void Numbers ( int a, int b ) {
+        if (a >= b) {
+            System.out.println("a>=b");
+        } else if (a < b){
+            System.out.println("a<b");
+        }
+    }
+
+
 }
